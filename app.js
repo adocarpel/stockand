f380992baa4b2ce88da440aed9404d560ed1d051
adocarpel <adocarpel@gmail.com>
2023-01-02 20:39:26 -0500

    git add

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require ('express')
 const app = express()
+require('dotenv').config()
 app.get('/',(req,res)=>{
     console.log(`App rreessppplistening o`)
     res.send("hol nnddoo");
 })  
 
 const port = process.env.PORT||4000;
-app.listen(4000,()=>{
+app.listen(port,()=>{
 
     
     console.log(`App listening on ${port}`);
